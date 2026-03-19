bab5e191883dbf8d75970cc5b024f8755842a51b
Petchingzaza <89173892+Petchingzaza@users.noreply.github.com>
2026-03-19 12:08:35 +0000

    Phy Lang Mode

--- Physics/landing.js
+++ Physics/landing.js
@@ -1,4 +1,16 @@
 function goTo(course){
   event.stopPropagation();
   window.location.href = `course.html?course=${course}`;
+  const langSwitch = document.getElementById('langSwitch');
+  console.log(langSwitch.checked);
 }
+
+const switchEl = document.getElementById("langSwitch");
+
+switchEl.addEventListener("change", function() {
+
+  const lang = this.checked ? "inter" : "th";
+
+  console.log("current lang =", lang);
+
+});
