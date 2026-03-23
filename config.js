ec3fc8e278d3d6a0a8acf478d81ff7f9557f7f18
BeamSuvijak <beamsvj@gmail.com>
2026-03-23 18:16:38 +0700

    บอกวิธีใช้ api ใน config.js

--- config.js
+++ config.js
@@ -1,3 +1,6 @@
 const CONFIG = {
     API_URL: "https://skintania-api.skintania143.workers.dev"
 };
+// วิธีใช้ import config.js เข้ามา
+// จะมีตัวแปร API_URL
+// วิธีเรียก api_url/bucket_name?file=path_to_file
